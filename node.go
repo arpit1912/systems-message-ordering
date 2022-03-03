b09c4262c39134117c4c8de57b34babf5034a917
Krishnendu Sudheesh <krishnendusudheesh4@gmail.com>
2022-03-03 19:33:06 +0530

    added mutex for mutual exclusion

--- node.go
+++ node.go
@@ -13,7 +13,13 @@ const (
         SERVER_TYPE = "tcp"
 )
 
-var all_conn map[string] net.Conn
+type SafeConnections struct {
+	mu sync.Mutex
+	all_conn map[string] net.Conn
+}
+
+
+var safeConnections SafeConnections
 
 func RecieveMessage (wg *sync.WaitGroup, port string) {
 	defer wg.Done()
@@ -41,7 +47,9 @@ func RecieveMessage (wg *sync.WaitGroup, port string) {
 		}
 		id:= string(buffer[:mLen])
 		fmt.Println("Received Connection Request From ", id)
-		all_conn[id] = client_conn
+		safeConnections.mu.Lock()
+		safeConnections.all_conn[id] = client_conn
+		safeConnections.mu.Unlock()
 		go listenClient(client_conn)
 
 	}
@@ -87,7 +95,9 @@ func establishConnections(wg *sync.WaitGroup, clients_port []string, my_port str
 	for {
 		no_done:= 0
 		for _, port := range(clients_port) {
-			_, ok := all_conn[port]
+			safeConnections.mu.Lock()
+			_, ok := safeConnections.all_conn[port]
+			safeConnections.mu.Unlock()
 			if ok {
 				fmt.Println("Already a connection is present to - ", port)
 				no_done ++
@@ -96,7 +106,9 @@ func establishConnections(wg *sync.WaitGroup, clients_port []string, my_port str
 				if err != nil {
 					fmt.Println("Error occured in connection: ")
 				} else {
-					all_conn[port] = conn
+					safeConnections.mu.Lock()
+					safeConnections.all_conn[port] = conn
+					safeConnections.mu.Unlock()
 					_, err = conn.Write([]byte(my_port)) 
 					go listenClient(conn)
 					if err != nil {
@@ -122,14 +134,15 @@ func BroadCastMessage(wg *sync.WaitGroup, my_port string) {
 
 
 	for {
-		for _, conn := range all_conn {
+		for v, conn := range safeConnections.all_conn {
+			fmt.Println("Sending Message to -" , v)
 			msg := "MSG FROM - " + my_port
 			_, err := conn.Write([]byte(msg))
 			if err != nil {
 				panic("Error sending message ;( ")
 			}
 		}
-		r := rand.Intn(5)
+		r := rand.Intn(10) + 3
 		time.Sleep(time.Duration(r) * time.Second)		
 	}
 	
@@ -138,7 +151,8 @@ func BroadCastMessage(wg *sync.WaitGroup, my_port string) {
 func main() {
 	var wg sync.WaitGroup
 	wg.Add(3)
-	all_conn = make(map[string] net.Conn)
+	safeConnections = SafeConnections{all_conn : make(map[string] net.Conn)}
+	
 	go RecieveMessage(&wg, os.Args[1])
 	time.Sleep(5*time.Second)
 	clients_port := os.Args[2:]
